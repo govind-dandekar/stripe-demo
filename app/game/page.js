8a3836652888cc0379fff6c4043379a8b28ea824
Govind Dandekar <govind.dandekar@gmail.com>
2025-05-13 18:04:29 -0700

    delete extra files

--- app/game/page.js
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-
-function GamePage() {
-  // redirect users to '/' if they manually input '/game'
-  return redirect("/");
-}
-
-export default GamePage;
--- components/custom-footer.js
+++ components/custom-footer.js
@@ -16,13 +16,6 @@ function CustomFooter() {
 
   return (
     <div className="flex text-white bg-cyan-400 m-3 py-3 px-3 mt-5 rounded-2xl font-sans justify-evenly align-middle w-3/4 font-thin text-xs lg:text-base">
-      <Link href={"/bluey-characters"} className="max-md:hidden">
-        <p>bluey characters</p>
-      </Link>
-      <Link href="/game-reviews" className="max-md:hidden">
-        <p>game reviews</p>
-      </Link>
-      <CustomFooterClient handleClick={scoresRedirect} />
       <Link href="https://github.com/govind-dandekar/react-nextjs-quiz-game/">
         <FaGithub className="scale-150 mt-1" />
       </Link>

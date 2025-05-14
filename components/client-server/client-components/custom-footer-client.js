650bcf85215a813fe6f4cbae5a6b979199bff879
Govind Dandekar <govind.dandekar@gmail.com>
2025-05-14 08:56:45 -0700

    commit prior to adding address element

--- components/client-server/client-components/custom-footer-client.js
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function CustomerFooterClient({ handleClick }) {
-  return <p onClick={handleClick}>high scores</p>;
-}
--- components/client-server/client-components/results-client.js
+++
@@ -1,6 +0,0 @@
-"use client";
-import SubmitButton from "../../ui/submit-button";
-
-export default function ResultsClient({ handleClick }) {
-  return <SubmitButton onClick={handleClick}>See High Scores!</SubmitButton>;
-}

5c0abce92a65e8f12cf35d0b28a17b4b581d3a86
Govind Dandekar <govind.dandekar@gmail.com>
2025-05-13 18:07:43 -0700

    update CustomFooter

--- components/custom-footer.js
+++ components/custom-footer.js
@@ -2,18 +2,7 @@ import Link from "next/link";
 
 import { FaHome, FaGithub } from "react-icons/fa";
 
-import CustomFooterClient from "@/components/client-server/client-components/custom-footer-client";
-
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-
 function CustomFooter() {
-  async function scoresRedirect() {
-    "use server";
-    revalidatePath("/scores");
-    redirect("/scores");
-  }
-
   return (
     <div className="flex text-white bg-cyan-400 m-3 py-3 px-3 mt-5 rounded-2xl font-sans justify-evenly align-middle w-3/4 font-thin text-xs lg:text-base">
       <Link href="https://github.com/govind-dandekar/react-nextjs-quiz-game/">
